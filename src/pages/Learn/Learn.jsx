47ff86d516bbc4a7c324c3adc98d59bfdddea83b
Marcos <msmarcondes10@gmail.com>
2024-12-09 18:26:18 -0300

    refactor: remove unused stage and bonusStage definitions in Learn component; repositions LifeMeter component

--- src/pages/Learn/Learn.jsx
+++ src/pages/Learn/Learn.jsx
@@ -26,98 +26,6 @@ function Learn() {
       left: 1,
       open: true,
     },
-    {
-      id: 1,
-      icon: "iconoir:heart-solid",
-      iconWidth: 50,
-      top: 27,
-      left: 2.5,
-      open: false,
-    },
-    {
-      id: 2,
-      icon: "bi:phone-fill",
-      iconWidth: 50,
-      top: 54,
-      left: 5.8,
-      open: false,
-    },
-    { id: 3, top: 80, left: 15, open: false },
-    { id: 4, top: 80, left: 23, open: false },
-    { id: 5, icon: "mdi:bomb", iconWidth: 65, top: 66, left: 27, open: false },
-    { id: 6, top: 56, left: 35, open: false },
-    { id: 7, top: 40, left: 38, open: false },
-    { id: 8, top: 28, left: 45, open: false },
-    { id: 9, top: 28, left: 54, open: false },
-    {
-      id: 10,
-      icon: "bi:phone-fill",
-      iconWidth: 50,
-      top: 43,
-      left: 61,
-      open: false,
-    },
-    {
-      id: 11,
-      top: 73,
-      left: 70,
-      open: false,
-    },
-    {
-      id: 12,
-      icon: "mdi:dice-multiple",
-      iconWidth: 60,
-      top: 81,
-      left: 76,
-      open: false,
-    },
-    {
-      id: 13,
-      top: 80,
-      left: 86,
-      open: false,
-    },
-    {
-      id: 14,
-      top: 65,
-      left: 91,
-      open: false,
-    },
-    {
-      id: 15,
-      top: 37,
-      left: 92,
-      final: true,
-    },
-  ];
-
-  const bonusStages = [
-    {
-      id: 1,
-      type: "accessory",
-      iconColor: "black",
-      color: "green",
-      top: 30,
-      left: 20,
-      open: false,
-    },
-    {
-      id: 2,
-      type: "accessory",
-      color: "yellow",
-      iconColor: "black",
-      top: 63,
-      left: 48,
-      open: false,
-    },
-    {
-      id: 3,
-      type: "accessory",
-      color: "pink",
-      top: 40,
-      left: 75,
-      open: false,
-    },
   ];
 
   const nextArrow = (
@@ -194,7 +102,7 @@ function Learn() {
 
 function LifeMeter({ lives = 3 }) {
   return (
-    <div className="absolute bottom-5 left-32 flex h-fit w-fit gap-2 rounded-sm bg-secondary-200/20 p-3">
+    <div className="absolute bottom-5 left-10 flex h-fit w-fit gap-2 rounded-sm bg-secondary-200/20 p-3">
       <Icon
         icon={"mdi:thunder"}
         width={48}

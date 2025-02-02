75373473f69c6e6a8a7a793fa019c6886806eb89
wesley <wesley.rochao@hotmail.com>
2025-02-01 21:52:36 -0300

    fix: Remove frontend client base path from configuration and router

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/vouvi-frontend-client">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>,

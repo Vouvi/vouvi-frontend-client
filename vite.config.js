719a5cc129a0e1089decc0c75341bf15484920b7
wesley <wesley.rochao@hotmail.com>
2025-10-26 23:04:17 -0300

    fix: Update homepage URL in package.json and simplify Vite config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => ({
-  base: command === 'build' ? '/vouvi-frontend-client/' : '/',
+export default defineConfig(() => ({
   plugins: [react()],
 }))
